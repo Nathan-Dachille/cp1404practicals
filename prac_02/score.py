4ed36f3cdf5bf4ffee3ff960186bace33805ce6b
Nathan-Dachille <77665531+Nathan-Dachille@users.noreply.github.com>
2023-09-19 17:17:27 +1000

    Implemented test of random score

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,11 +3,17 @@ CP1404/CP5632 - Practical
 Program to determine score status
 """
 
+from random import randint
+
 
 def main():
     """Asks for a score and rates it."""
     score = int(input("Enter score: "))
     print(test_score(score))
+    random_score = randint(0, 100)
+    print(f"The random score is {random_score}.")
+    random_result = test_score(random_score)
+    print(f"The result of the random score is '{random_result}'.")
 
 
 def test_score(score: int) -> str:
