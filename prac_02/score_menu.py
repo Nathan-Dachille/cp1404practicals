2da565e9aa5ab7721315580a7aa4d0da5482b33e
Nathan-Dachille <77665531+Nathan-Dachille@users.noreply.github.com>
2023-09-19 17:36:01 +1000

    Copy test_score function from score.py

---
+++ prac_02/score_menu.py
@@ -0,0 +1,23 @@
+"""Gets a valid score, prints a result and shows stars representing the score."""
+
+
+def main():
+    pass
+
+
+def test_score(score: int) -> str:
+    """Tests a score between 0 and 100 and returns a result."""
+    if score < 0:
+        result = "Invalid score"
+    elif score < 50:
+        result = "Bad"
+    elif score < 90:
+        result = "Passable"
+    elif score <= 100:
+        result = "Excellent"
+    else:
+        result = "Invalid score"
+    return result
+
+
+main()
