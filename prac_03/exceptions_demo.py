69cdd85d4f9994ae0fd637a0499983a6ae7dbe95
ModdedManiac013 <77665531+Nathan-Dachille@users.noreply.github.com>
2023-09-27 13:45:22 +1000

    Answered questions in exceptions_demo.py

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -2,8 +2,11 @@
 CP1404/CP5632 - Practical
 Answer the following questions:
 1. When will a ValueError occur?
+    A ValueError will occur when the input is not a number.
 2. When will a ZeroDivisionError occur?
+    When denominator is 0.
 3. Could you change the code to avoid the possibility of a ZeroDivisionError?
+    Don't allow the denominator to be equal to 0.
 """
 
 try:
