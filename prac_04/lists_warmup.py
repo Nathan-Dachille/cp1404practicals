f8aa334fd07088e6808ef1d898f5a46eb75bf86f
ModdedManiac013 <77665531+Nathan-Dachille@users.noreply.github.com>
2023-10-02 10:48:18 +1000

    Complete lists_warmup.py

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -9,3 +9,8 @@ numbers = [3, 1, 4, 1, 5, 9, 2]
 # 7 in numbers == 0
 # "3" in numbers == 0
 # numbers + [6, 5, 3] == [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+numbers[0] = "ten"  # type: ignore
+numbers[-1] = 1
+print(numbers[2:])
+print("9 is an element of numbers:", bool(9 in numbers))
