a8acc40ff3ce28f2a604e4157314fbc24d62d04c
Nathan D'Achille <ndachille12@outlook.com>
2023-10-08 16:03:51 +1000

    Fix missing space in hex_colours.py

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -3,7 +3,7 @@ CP1404/CP5632 Practical
 Hex colours in a dictionary
 """
 
-COLOUR_TO_HEX = {"red1": "#ff0000","maroon": "#b03060", "orange1": "#ffa500", "yellow1": "#ffff00",
+COLOUR_TO_HEX = {"red1": "#ff0000", "maroon": "#b03060", "orange1": "#ffa500", "yellow1": "#ffff00",
                  "green1": "#00ff00", "darkgreen": "#006400", "cyan1": "#00ffff", "blue1": "#0000ff",
                  "indigo": "#4b0082", "violet": "#ee82ee"}
 
