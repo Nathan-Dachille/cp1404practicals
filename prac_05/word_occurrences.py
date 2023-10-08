82a0e3292214d3a22c076ef4d20caef9047604fd
Nathan D'Achille <ndachille12@outlook.com>
2023-10-08 16:26:43 +1000

    Complete word_occurrences.py

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,4 +1,20 @@
 """Counts the occurrences of if words in a string.
 Estimate: 20 minutes
-Actual:
+Actual:   16 minutes
 """
+
+text = input("Enter a string: ").lower()
+
+words = text.split(" ")
+
+word_to_count = dict()
+
+for word in words:
+    if word not in word_to_count:
+        word_to_count[word] = 1
+    else:
+        word_to_count[word] += 1
+
+print(f"Text: {text}")
+for word in word_to_count:
+    print(f"{word} : {word_to_count[word]}")
