455a01a4d72f19d4bc00660086e455c59b597c20
Nathan D'Achille <77665531+Nathan-Dachille@users.noreply.github.com>
2023-10-17 20:49:08 +1000

    Finish programming_language.py and languages.py

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -2,3 +2,18 @@
 Estimated time: 20min
 Actual time:
 """
+
+
+from prac_06.programming_language import ProgrammingLanguage
+
+
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+print(python)
+
+languages = [python, ruby, visual_basic]
+
+for language in languages:
+    if language.is_dynamic():
+        print(language.name)
