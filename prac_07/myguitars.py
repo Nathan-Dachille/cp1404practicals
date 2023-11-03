5ff4e30ef35307b399f10c4002aaa0e790efb7a6
Nathan D'Achille <77665531+Nathan-Dachille@users.noreply.github.com>
2023-11-03 11:49:49 +1000

    Implements the ability to generate new guitar objects from valid user input

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -7,19 +7,45 @@ FILENAME = "guitars.csv"
 
 
 def main():
-    """Reads CSV, generates Guitar objects and displays."""
-    guitars = []
+    """Gets new guitar objects from user input."""
+    guitars = read_guitars()
 
+    print("My Guitars!")
+    name = input("Name: ")
+    while name != "":
+        is_valid_cost = False
+        is_valid_year = False
+        while not is_valid_year:
+            try:
+                year = int(input("Year: "))
+                is_valid_year = True
+            except ValueError:
+                print("Invalid year!")
+        while not is_valid_cost:
+            try:
+                cost = float(input("Cost: $"))
+                is_valid_cost = True
+            except ValueError:
+                print("Invalid cost!")
+        guitars.append(Guitar(name, year, cost))
+        print(guitars[-1], "added.")
+        name = input("Name: ")
+
+    guitars.sort()
+    for guitar in guitars:
+        print(guitar)
+
+
+def read_guitars():
+    """Reads CSV and returns list of Guitar objects."""
+    guitars = []
     with open(FILENAME, 'r', newline='') as in_file:
         in_file.readline()
         reader = csv.reader(in_file)
         for row in reader:
             guitar = Guitar(row[0], int(row[1]), float(row[2]))
             guitars.append(guitar)
-
-    guitars.sort()
-    for guitar in guitars:
-        print(guitar)
+    return guitars
 
 
 if __name__ == '__main__':
