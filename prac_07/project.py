6147c5b2bc1d2bab0f664b3101bc22c971b99e2f
Nathan D'Achille <77665531+Nathan-Dachille@users.noreply.github.com>
2023-11-03 12:56:19 +1000

    Add __str__ method to Project class

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,9 +1,17 @@
 """Project Class for Project Management Program Exercise."""
 
+
 class Project:
+    """Stores Project data."""
     def __init__(self, name, start_date, priority=1, cost_estimate=10.1, completion_percentage=0):
+        """Initialise Project object."""
         self.name = name
         self.start_date = start_date
         self.priority = priority
         self.cost_estimate = cost_estimate
         self.completion_percentage = completion_percentage
+
+    def __str__(self):
+        """Returns string representing Project data."""
+        return (f"{self.name}, start:{self.start_date}, priority {self.priority}, "
+                f"estimate: ${self.cost_estimate}, completion: {self.completion_percentage}%")
