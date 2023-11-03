b8648b90fbe2b559152b7b088ad1b98fec26033f
Nathan D'Achille <77665531+Nathan-Dachille@users.noreply.github.com>
2023-11-03 11:31:24 +1000

    Update Docstrings in guitar.py

--- prac_07/guitar.py
+++ prac_07/guitar.py
@@ -2,20 +2,22 @@
 
 
 class Guitar:
+    """Represent information about guitars."""
 
     def __init__(self, name="", year=0, cost=0):
+        """Initialise a Guitar object."""
         self.name = name
         self.year = year
         self.cost = cost
 
     def __str__(self):
+        """Return information as a string."""
         return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
 
     def get_age(self):
+        """Return age of Guitar object."""
         return 2023 - self.year
 
     def is_vintage(self):
-        if self.get_age() >= 50:
-            return True
-        else:
-            return False
+        """Return boolean True is age is greater than or equal to 50 years."""
+        return self.get_age() >= 50
