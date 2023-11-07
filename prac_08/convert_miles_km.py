489f1b00a75b58b3c63f1b1271130ecd02e2288f
Nathan D'Achille <77665531+Nathan-Dachille@users.noreply.github.com>
2023-11-07 17:24:07 +1000

    Handle increment and decrement

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,12 +1,22 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import StringProperty
 
 
 class ConvertMilesKm(App):
+
+    output_distance = StringProperty()
+
     def build(self):
         self.title = "Convert Miles to Kilometers"
         self.root = Builder.load_file('convert_miles_km.kv')
+        self.output_distance = "Enter a distance and press enter"
         return self.root
+
+    def handle_increment(self, increment):
+        current_input = int(self.root.ids.input_distance.text)
+        current_input += increment
+        self.root.ids.input_distance.text = str(current_input)
 
 
 ConvertMilesKm().run()
