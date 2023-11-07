60fab0f6d7fe0e5b31d69e18ddd882a391f07986
Nathan D'Achille <77665531+Nathan-Dachille@users.noreply.github.com>
2023-11-07 18:22:59 +1000

    Update docstrings in convert_miles_km.py

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,3 +1,4 @@
+"""Converting miles to kilometers exercise."""
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.properties import StringProperty
@@ -6,15 +7,18 @@ MILES_KM_CONVERSION_FACTOR = 1.61
 
 
 class ConvertMilesKm(App):
+    """Kivy app that converts miles to Kilometers."""
     output_distance = StringProperty()
 
     def build(self):
+        """Build the Kivy App."""
         self.title = "Convert Miles to Kilometers"
         self.root = Builder.load_file('convert_miles_km.kv')
         self.output_distance = "Enter a distance and press enter"
         return self.root
 
     def handle_increment(self, increment):
+        """Increment and decrement input with error checking."""
         try:
             current_input = float(self.root.ids.input_distance.text)
         except ValueError:
@@ -24,6 +28,7 @@ class ConvertMilesKm(App):
         self.root.ids.input_distance.text = str(current_input)
 
     def convert(self):
+        """Convert from miles to kilometers using MILES_KM_CONVERSION_FACTOR."""
         try:
             current_input = float(self.root.ids.input_distance.text)
             current_input *= MILES_KM_CONVERSION_FACTOR
