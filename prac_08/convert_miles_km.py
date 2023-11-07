74b1eff084fac28052391161d703d5e515dc3c92
Nathan D'Achille <77665531+Nathan-Dachille@users.noreply.github.com>
2023-11-07 17:37:10 +1000

    Complete Miles to Kilometres Converter exercise

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -2,9 +2,10 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.properties import StringProperty
 
+MILES_KM_CONVERSION_FACTOR = 1.61
+
 
 class ConvertMilesKm(App):
-
     output_distance = StringProperty()
 
     def build(self):
@@ -14,9 +15,14 @@ class ConvertMilesKm(App):
         return self.root
 
     def handle_increment(self, increment):
-        current_input = int(self.root.ids.input_distance.text)
+        current_input = float(self.root.ids.input_distance.text)
         current_input += increment
         self.root.ids.input_distance.text = str(current_input)
+
+    def convert(self):
+        current_input = float(self.root.ids.input_distance.text)
+        current_input *= MILES_KM_CONVERSION_FACTOR
+        self.output_distance = str(current_input)
 
 
 ConvertMilesKm().run()
