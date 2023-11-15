0d3b1264c149007494a6017ece3cb8c07323f3a7
Nathan D'Achille <77665531+Nathan-Dachille@users.noreply.github.com>
2023-11-15 16:20:03 +1000

    Create silver_service_taxi_test.py to test SilverServiceTaxi class

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -1,3 +1,11 @@
 """Program for testing SilverServiceTaxi class."""
 
+from silver_service_taxi import SilverServiceTaxi
+
+silver_service_taxi = SilverServiceTaxi("Hummer", 100, fanciness=2)
+print(silver_service_taxi)
+silver_service_taxi.start_fare()
+silver_service_taxi.drive(18)
+print(silver_service_taxi)
+print(f"Current fare: {silver_service_taxi.get_fare()}")
 
