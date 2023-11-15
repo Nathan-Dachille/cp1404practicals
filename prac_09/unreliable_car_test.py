920bd6c3ceed0a1b0a8a828b9bb7a62c58046e70
Nathan D'Achille <77665531+Nathan-Dachille@users.noreply.github.com>
2023-11-15 15:56:53 +1000

    Test UnreliableCar class with test program

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -1,3 +1,8 @@
 """Program for testing UnreliableCar class."""
+from prac_09.unreliable_car import UnreliableCar
 
 
+unreliable_car = UnreliableCar("Car 1", 100, 50)
+print(unreliable_car)
+unreliable_car.drive(40)
+print(unreliable_car)
